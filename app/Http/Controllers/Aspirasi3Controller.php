dc771a0fcb590565269d4600aa645e9ff96d481f
Imelda134 <imeldamayantisipit@gmail.com>
2022-03-19 17:25:59 +0700

    percobaan

---
+++ app/Http/Controllers/Aspirasi3Controller.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aspirasi3;
+use App\Http\Requests\StoreAspirasi3Request;
+use App\Http\Requests\UpdateAspirasi3Request;
+
+class Aspirasi3Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAspirasi3Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAspirasi3Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Aspirasi3  $aspirasi3
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Aspirasi3 $aspirasi3)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Aspirasi3  $aspirasi3
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Aspirasi3 $aspirasi3)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAspirasi3Request  $request
+     * @param  \App\Models\Aspirasi3  $aspirasi3
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAspirasi3Request $request, Aspirasi3 $aspirasi3)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Aspirasi3  $aspirasi3
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Aspirasi3 $aspirasi3)
+    {
+        //
+    }
+}
